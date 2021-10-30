6c7a289665a6d657db9201be079cb0003404a9fd
Jari Beguš <someone755@users.noreply.github.com>
2021-10-30 19:45:18 +0200

    bram.v: create single-port memory field in BRAM

---
+++ art.srcs/sources_1/bram.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+
+module bram
+	#(
+	parameter p_RAM_WIDTH = 8,	// word size
+	parameter p_RAM_DEPTH = 32	// number of addresses
+	)
+	(
+	input IN_CLK,
+	input [$clog2(p_RAM_DEPTH)-1:0] IN_BRAM_ADDR,
+	input [p_RAM_WIDTH-1:0] IN_BRAM_DATAIN,
+	input IN_BRAM_WRITE_EN,
+	
+	output [p_RAM_WIDTH-1:0] OUT_BRAM_DATAOUT
+	);
+
+(* ram_style = "block" *) reg [p_RAM_WIDTH-1:0] rN_ram [p_RAM_DEPTH-1:0];
+
+reg [p_RAM_WIDTH-1:0] r_OUT_BRAM_DATAOUT, r_ram_data;
+assign OUT_BRAM_DATAOUT = r_OUT_BRAM_DATAOUT;
+
+// Xilinx claims a register will not be absorbed into BRAM when its initial value
+//	is not declared as 0. The below initial block is supposedly synthesizable, but
+//	unnecessary; Vivado's synthesis reports rN_ram is absorbed properly into BRAM
+// and automatically initialized to 0. 
+// https://support.xilinx.com/s/article/64049?language=en_US
+//initial begin: init_to_zero
+//	integer i = 0;
+//	for (i=0; i<p_RAM_DEPTH; i=i+1)
+//		rN_ram[i] = {p_RAM_WIDTH{1'b0}};
+//end // init_to_zero
+
+always @(posedge IN_CLK) begin: bram
+	if ( IN_BRAM_WRITE_EN )
+		rN_ram[IN_BRAM_ADDR] = IN_BRAM_DATAIN; 
+	
+	r_OUT_BRAM_DATAOUT = rN_ram[IN_BRAM_ADDR]; 
+end // bram
+
+endmodule
