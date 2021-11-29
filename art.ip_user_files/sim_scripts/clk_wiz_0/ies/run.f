4965f92603f92daaee567960e670d155a82d465a
Jari Beguš <someone755@users.noreply.github.com>
2021-11-30 00:35:57 +0100

    clk_wiz: Use Vivado clocking wizard to create 200 MHz clock
    Needed for MIG IP core.

---
+++ art.ip_user_files/sim_scripts/clk_wiz_0/ies/run.f
@@ -0,0 +1,14 @@
+-makelib ies_lib/xpm -sv \
+  "C:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "C:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../../../art_ip/clk_wiz_0/clk_wiz_0_clk_wiz.v" \
+  "../../../../../art_ip/clk_wiz_0/clk_wiz_0.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
