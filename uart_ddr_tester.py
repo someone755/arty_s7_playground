571c367d3a8b4aa20a5125bd07b16243ba3a410c
Jari Beguš <someone755@users.noreply.github.com>
2021-12-10 16:31:14 +0100

    uart_ddr_tester: BL8 demands 16 byte r/w operation
    * Also disregard speed calculation for small data chunks
    (reading from RX buffer is nearly instant)

--- uart_ddr_tester.py
+++ uart_ddr_tester.py
@@ -3,7 +3,7 @@ serialPort = 'COM5'
 # Baud rate set in Artix bitstream
 baudRate = 12000000
 # Size of data to write to DDR3, in bytes
-# Must be multiple of 8, up to module size
+# Must be multiple of 16, up to module size
 # of 2 Gbit or 2**28 = 268435456 bytes
 sizeOfData = 2**27+8
 # Whether to wait for ACK upon TX
@@ -29,7 +29,7 @@ if flushDDR:
 
     # Make sure DDR is empty by flushing with EOF word
     print('[{}] I: Flushing DDR with EOF word...'.format(time.time()-t))
-    for i in range(0,16):
+    for i in range(0,32):
         ser.write(b'\x66')
         ackByte = ser.read()
         if (ackByte != b'\x8a') and (ackByte != b''):
@@ -39,12 +39,12 @@ if flushDDR:
 
     # Close and reopen serial port; this drops the current TX buffer
     ser.close()
-ser = serial.Serial(serialPort, baudRate)
+ser = serial.Serial(serialPort, baudRate, timeout=0.1)
 ser.set_buffer_size(rx_size = sizeOfData, tx_size = sizeOfData)
 
 # Check data generation
-if sizeOfData % 8 :
-    print('[{}] E: Data size must be multiple of 8!'.format(time.time()-t))
+if sizeOfData % 16 :
+    print('[{}] E: Data size must be multiple of 16!'.format(time.time()-t))
     quit()
 
 # Check if file exists
@@ -61,22 +61,23 @@ dataTx = open('dataTx.bin','rb').read()
 # Generate md5 hash
 dataTxHash = hashlib.md5(dataTx).hexdigest()
 
-# Send 8 bytes, wait for ack
+# Send 16 bytes, wait for ack
 print('[{}] I: TX data blob...'.format(time.time()-t))
 time1 = time.time()
 if (waitForAck):
-    for i in range (0, sizeOfData, 8):
-        ser.write(dataTx[i:i+8])
+    for i in range (0, sizeOfData, 16):
+        ser.write(dataTx[i:i+16])
         #ackByte = ser.read()
         # print('{} I: ACK byte is'.format(i/8), ackByte)#, end =" ")
-        ackByte = ser.read()
+        ackByte = ser.read(1)
         if ackByte != b'\x8a':
             print(i,"[{}] E: RX byte b'x8a' != ".format(time.time()-t), ackByte)
             quit()
 else:
     ser.write(dataTx)
 time2 = time.time()
-print('[{}] I: TX effective rate:'.format(time2-t),sizeOfData*8/(time2-time1)/1000,'kbit/s')
+if (time2-time1) != 0:
+    print('[{}] I: TX effective rate:'.format(time2-t),sizeOfData*8/(time2-time1)/1000,'kbit/s')
 time.sleep(2)
 
 # Close and reopen serial port; this drops the current TX buffer
@@ -86,7 +87,7 @@ ser.set_buffer_size(rx_size = sizeOfData, tx_size = sizeOfData)
 
 # Send 64-bit EOF signal
 print('[{}] I: TX EOF word..'.format(time.time()-t))
-for i in range (0,8):
+for i in range (0,16):
     ser.write(b'\x66')
 time.sleep(0.1)
 
@@ -95,7 +96,8 @@ print('[{}] I: Receiving data from DDR...'.format(time.time()-t))
 time1 = time.time()
 dataRx = ser.read(sizeOfData)
 time2 = time.time()
-print('[{}] I: RX effective rate:'.format(time2-t),len(dataRx)*8/(time2-time1)/1000,'kbit/s')
+if (time2-time1) != 0:
+    print('[{}] I: RX effective rate:'.format(time2-t),len(dataRx)*8/(time2-time1)/1000,'kbit/s')
 open('dataRx.bin', 'wb').write(dataRx)
 
 # Generate MD5 for RX data
