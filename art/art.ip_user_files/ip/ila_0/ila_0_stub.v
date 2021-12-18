aeb4f9743b3ea2cac058819f1572fcf0d660680c
Jari Beguš <someone755@users.noreply.github.com>
2021-12-18 01:47:30 +0100

    ILA: Update to latest probe settings

--- art/art.ip_user_files/ip/ila_0/ila_0_stub.v
+++ art/art.ip_user_files/ip/ila_0/ila_0_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
-// Date        : Sun Dec 12 16:38:25 2021
+// Date        : Fri Dec 17 23:37:40 2021
 // Host        : Aeternitas running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
 //               C:/Users/jarib/OneDrive/work/mag/vivado-proj/art_ip/ila_0/ila_0_stub.v
@@ -15,27 +15,20 @@
 // Please paste the declaration into a Verilog source file or add the file as an additional source.
 (* X_CORE_INFO = "ila,Vivado 2019.2" *)
 module ila_0(clk, trig_in, trig_in_ack, probe0, probe1, probe2, 
-  probe3, probe4, probe5, probe6, probe7, probe8, probe9, probe10, probe11, probe12, probe13, probe14, 
-  probe15, probe16)
-/* synthesis syn_black_box black_box_pad_pin="clk,trig_in,trig_in_ack,probe0[63:0],probe1[63:0],probe2[127:0],probe3[127:0],probe4[0:0],probe5[0:0],probe6[0:0],probe7[0:0],probe8[127:0],probe9[0:0],probe10[0:0],probe11[0:0],probe12[27:0],probe13[0:0],probe14[0:0],probe15[3:0],probe16[0:0]" */;
+  probe3, probe4, probe5, probe6, probe7, probe8, probe9, probe10)
+/* synthesis syn_black_box black_box_pad_pin="clk,trig_in,trig_in_ack,probe0[127:0],probe1[127:0],probe2[0:0],probe3[0:0],probe4[0:0],probe5[0:0],probe6[0:0],probe7[27:0],probe8[27:0],probe9[127:0],probe10[127:0]" */;
   input clk;
   input trig_in;
   output trig_in_ack;
-  input [63:0]probe0;
-  input [63:0]probe1;
-  input [127:0]probe2;
-  input [127:0]probe3;
+  input [127:0]probe0;
+  input [127:0]probe1;
+  input [0:0]probe2;
+  input [0:0]probe3;
   input [0:0]probe4;
   input [0:0]probe5;
   input [0:0]probe6;
-  input [0:0]probe7;
-  input [127:0]probe8;
-  input [0:0]probe9;
-  input [0:0]probe10;
-  input [0:0]probe11;
-  input [27:0]probe12;
-  input [0:0]probe13;
-  input [0:0]probe14;
-  input [3:0]probe15;
-  input [0:0]probe16;
+  input [27:0]probe7;
+  input [27:0]probe8;
+  input [127:0]probe9;
+  input [127:0]probe10;
 endmodule
--- art/art.ip_user_files/ip/ila_0/ila_0_stub.vhdl
+++ art/art.ip_user_files/ip/ila_0/ila_0_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
--- Date        : Sun Dec 12 16:38:25 2021
+-- Date        : Fri Dec 17 23:37:40 2021
 -- Host        : Aeternitas running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub
 --               C:/Users/jarib/OneDrive/work/mag/vivado-proj/art_ip/ila_0/ila_0_stub.vhdl
@@ -17,23 +17,17 @@ entity ila_0 is
     clk : in STD_LOGIC;
     trig_in : in STD_LOGIC;
     trig_in_ack : out STD_LOGIC;
-    probe0 : in STD_LOGIC_VECTOR ( 63 downto 0 );
-    probe1 : in STD_LOGIC_VECTOR ( 63 downto 0 );
-    probe2 : in STD_LOGIC_VECTOR ( 127 downto 0 );
-    probe3 : in STD_LOGIC_VECTOR ( 127 downto 0 );
+    probe0 : in STD_LOGIC_VECTOR ( 127 downto 0 );
+    probe1 : in STD_LOGIC_VECTOR ( 127 downto 0 );
+    probe2 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe3 : in STD_LOGIC_VECTOR ( 0 to 0 );
     probe4 : in STD_LOGIC_VECTOR ( 0 to 0 );
     probe5 : in STD_LOGIC_VECTOR ( 0 to 0 );
     probe6 : in STD_LOGIC_VECTOR ( 0 to 0 );
-    probe7 : in STD_LOGIC_VECTOR ( 0 to 0 );
-    probe8 : in STD_LOGIC_VECTOR ( 127 downto 0 );
-    probe9 : in STD_LOGIC_VECTOR ( 0 to 0 );
-    probe10 : in STD_LOGIC_VECTOR ( 0 to 0 );
-    probe11 : in STD_LOGIC_VECTOR ( 0 to 0 );
-    probe12 : in STD_LOGIC_VECTOR ( 27 downto 0 );
-    probe13 : in STD_LOGIC_VECTOR ( 0 to 0 );
-    probe14 : in STD_LOGIC_VECTOR ( 0 to 0 );
-    probe15 : in STD_LOGIC_VECTOR ( 3 downto 0 );
-    probe16 : in STD_LOGIC_VECTOR ( 0 to 0 )
+    probe7 : in STD_LOGIC_VECTOR ( 27 downto 0 );
+    probe8 : in STD_LOGIC_VECTOR ( 27 downto 0 );
+    probe9 : in STD_LOGIC_VECTOR ( 127 downto 0 );
+    probe10 : in STD_LOGIC_VECTOR ( 127 downto 0 )
   );
 
 end ila_0;
@@ -42,7 +36,7 @@ architecture stub of ila_0 is
 attribute syn_black_box : boolean;
 attribute black_box_pad_pin : string;
 attribute syn_black_box of stub : architecture is true;
-attribute black_box_pad_pin of stub : architecture is "clk,trig_in,trig_in_ack,probe0[63:0],probe1[63:0],probe2[127:0],probe3[127:0],probe4[0:0],probe5[0:0],probe6[0:0],probe7[0:0],probe8[127:0],probe9[0:0],probe10[0:0],probe11[0:0],probe12[27:0],probe13[0:0],probe14[0:0],probe15[3:0],probe16[0:0]";
+attribute black_box_pad_pin of stub : architecture is "clk,trig_in,trig_in_ack,probe0[127:0],probe1[127:0],probe2[0:0],probe3[0:0],probe4[0:0],probe5[0:0],probe6[0:0],probe7[27:0],probe8[27:0],probe9[127:0],probe10[127:0]";
 attribute X_CORE_INFO : string;
 attribute X_CORE_INFO of stub : architecture is "ila,Vivado 2019.2";
 begin
