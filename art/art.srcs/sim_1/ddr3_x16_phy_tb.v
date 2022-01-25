f7f93537ada04ec5cab419c25e4175e3abd46af9
Jari Beguš <someone755@users.noreply.github.com>
2022-01-26 00:26:37 +0100

    ddr3_x16_phy: Fix SERDES implementation, simulate READ op
    * phy: Drop slow clock for PHY -- main clock is DDR clock
    * phy: Attempt READ op fix
    * phy: General variable readability and comment update
    
    * tb: Correctly clock OSERDES parallel data input (5 CK delay)
    * tb: Generate DQS/DQ "READ" signals to test ISERDES
    * tb TODO: cleanup variable names for readability
    
    * wcfg: Source objects directly from PHY (ignore dirty _tb naming convention)

--- art/art.srcs/sim_1/ddr3_x16_phy_tb.v
+++ art/art.srcs/sim_1/ddr3_x16_phy_tb.v
@@ -11,14 +11,13 @@ localparam clk_ref_period	= 5;	// 200 MHz
 
 	reg clk_ddr_100;
 	reg clk_ddr_100_90deg;
-	reg clk_ui_25;
+	reg clk_ui_50;
 	reg clk_ref;
 	
-reg phy_rst = 1;
-	
+reg phy_rst = 1; 
 
 initial begin: phy_clk_gen // 100 MHz
-	clk_ddr_100 = 0;
+	clk_ddr_100 = 0; 
 	forever begin
 		#(ddr_ck_period/2) // delay half period
 		clk_ddr_100 = ~clk_ddr_100;
@@ -32,15 +31,16 @@ initial begin: phy_clk_90_gen // 100 MHz, phase shift
 		clk_ddr_100_90deg = ~clk_ddr_100_90deg;
 	end
 end
-initial begin: ui_clk_gen // 25 MHz
+initial begin: ui_clk_gen // 50 MHz
 	#25
-	clk_ui_25 = 0;
+	clk_ui_50 = 0;
 	forever begin
-		#20 // delay half period
-		clk_ui_25 = ~clk_ui_25;
+		#10 // delay half period
+		clk_ui_50 = ~clk_ui_50;
 	end
 end
 initial begin: clk_ref_gen // 200 MHz
+	#3
 	clk_ref = 0;
 	forever begin
 		#(clk_ref_period/2) // delay half period
@@ -59,40 +59,45 @@ wire	[1:0]	w2_dqs_out_nen;
 assign w2_dqs_out_nen[0] = r_dqs_out_nen;
 assign w2_dqs_out_nen[1] = r_dqs_out_nen;
 
-wire	[15:0]	w16_io_dq;
-assign w16_io_dq = {(16){1'bZ}};
+reg		[127:0]	dq_wr_data = 128'b0;
+wire	[63:0]	dq_rd_data_phyout;
+reg		[15:0]	dq_rd_data_tb_gen = 16'b0;
+wire	[15:0]	w16_io_dq = (r_dqs_out_nen) ? dq_rd_data_tb_gen : {(16){1'bZ}};
 
+reg r_dqs_rdstrobe_en = 0;
+
+wire	r_dqs_read = (r_dqs_rdstrobe_en) ? clk_ddr_100 : 0;
+wire	dqs_p_io = (r_dqs_out_nen) ? r_dqs_read : 1'bZ;
 wire	[1:0]	w2_io_dqs_p;
-assign w2_io_dqs_p = 2'bZZ;
+assign w2_io_dqs_p = {dqs_p_io, dqs_p_io};
 
+wire	dqs_n_io = (r_dqs_out_nen) ? ~r_dqs_read : 1'bZ;
 wire	[1:0]	w2_io_dqs_n;
-assign w2_io_dqs_n = 2'bZZ;
+assign w2_io_dqs_n = {dqs_n_io, dqs_n_io};
 
 ddr3_x16_phy #(
 	.REFCLK_FREQUENCY(200.0)
 ) phy_inst (
-	.i_clk_ui(clk_ui_25),	// 4:1 ratio between ui and ddr clocks
 	.i_clk_ddr(clk_ddr_100),
 	.i_clk_ddr_90(clk_ddr_100_90deg),
 	.i_clk_ref(clk_ref),	// 200 or 300 MHz, see REFCLK_FREQUENCY, used for IDELAYCTRL
 	
 	.i_phy_rst(phy_rst),	// active high reset for OSERDES, IDELAYCTRL
+	.i_phy_tristate_en(r_dqs_out_nen),
 	
-	.i128_phy_wrdata(128'h0000_1111_2222_3333_4444_5555_6666_7777),
+	.i128_phy_wrdata(dq_wr_data),
+	.o64_phy_rddata(dq_rd_data_phyout),
 	
-	.i2_phy_dqs(w2_dqs_in),
-	.i2_dqs_iobuf_out_nen(w2_dqs_out_nen),
-	.i_dq_oserdes_en(dq_oserdes_en),
-
+	.i_phy_dq_oserdes_en(dq_oserdes_en),
+	
+	.i_phy_dqs_oddr_d1en(r_dqs_in),
 
 	.io16_ddr_dq(w16_io_dq),
 	.io2_ddr_dqs_p(w2_io_dqs_p),
 	.io2_ddr_dqs_n(w2_io_dqs_n),
 	
 	.o_ddr_ck_p(),
-	.o_ddr_ck_n(),
-	
-	.o14_ddr_addr()
+	.o_ddr_ck_n()
 );
 
 initial begin: test
@@ -106,70 +111,39 @@ initial begin: test
 	#40
 	r_dqs_out_nen = 0;
 	#40
+	// See MR2 definition: Since our clock is always tCK > 2.5 ns, 
+	// CWL MUST be 5 CK. Hence #50 delay comes in handy. (10 ns is 100 MHz "DDR clock" period.)
+	// TODO I guess: replace fixed delays with "#ddr_ck_period"
+	dq_wr_data = 'haaaa_bbbb_cccc_dddd_eeee_ffff_1111_2222;
+	#50
 	r_dqs_in = 1;
 	dq_oserdes_en = 1;
-	#40
+	#40 
 	dq_oserdes_en = 0;
 	r_dqs_in = 0;
+	#40
+	#10	// what if read data strobe is not synchronous?
 	r_dqs_out_nen = 1;
-	#50
+	#40
+	r_dqs_rdstrobe_en = 1;
+	dq_rd_data_tb_gen = 16'haaaa;
+	#5
+	dq_rd_data_tb_gen = 16'hbbbb;
+	#5
+	dq_rd_data_tb_gen = 16'h3333;
+	#5
+	dq_rd_data_tb_gen = 16'h4444;
+	#5
+	dq_rd_data_tb_gen = 16'h5555;
+	#5
+	dq_rd_data_tb_gen = 16'h6666;
+	#5
+	dq_rd_data_tb_gen = 16'h7777;
+	#5
+	dq_rd_data_tb_gen = 16'h8888;
+	r_dqs_rdstrobe_en = 0;
+	#40
+	#40
 	$stop;
 end // test
-wire w_oserdes_out;
-reg oserdes_oce = 1'b0;
-OSERDESE2 #(
-		.DATA_RATE_OQ("DDR"), // DDR, SDR
-		.DATA_RATE_TQ("BUF"), // DDR, BUF, SDR
-		.DATA_WIDTH(8), // Parallel data width (2-8,10,14)
-		.TRISTATE_WIDTH(1), // 3-state converter width (1,4)
-		.SERDES_MODE("MASTER")
-	) oserdes_dq_inst (
-		.OFB(), // 1-bit output: Feedback path for data
-		.OQ(w_oserdes_out), // 1-bit output: Data path output
-		// SHIFTOUT1 / SHIFTOUT2: 1-bit (each) output: Data output expansion (1-bit each)
-		.SHIFTOUT1(),
-		.SHIFTOUT2(),
-		.TBYTEOUT(), // 1-bit output: Byte group tristate
-		.TFB(), // 1-bit output: 3-state control
-		.TQ(), // 1-bit output: 3-state control
-		.CLK(clk_ddr_100), // 1-bit input: High speed clock
-		.CLKDIV(clk_ui_25), // 1-bit input: Divided clock
-		// D1 - D8: 1-bit (each) input: Parallel data inputs (1-bit each)
-		.D1(1'b1),
-		.D2(1'b0),
-		.D3(1'b0),
-		.D4(1'b1),
-		.D5(1'b0),
-		.D6(1'b0),
-		.D7(1'b1),
-		.D8(1'b0),
-		.OCE(oserdes_oce), // 1-bit input: Output data clock enable
-		.RST(phy_rst), // 1-bit input: Reset
-		// SHIFTIN1 / SHIFTIN2: 1-bit (each) input: Data input expansion (1-bit each)
-		.SHIFTIN1(1'b0),
-		.SHIFTIN2(1'b0),
-		// T1 - T4: 1-bit (each) input: Parallel 3-state inputs
-		.T1(1'b1),//w2_dqs_iobuf_out_nen[i/8]),
-		.T2(),//w2_dqs_iobuf_out_nen[i/8]),
-		.T3(),//w2_dqs_iobuf_out_nen[i/8]),
-		.T4(),//w2_dqs_iobuf_out_nen[i/8]),
-		.TBYTEIN(1'b0), // 1-bit input: Byte group tristate
-		.TCE(1'b1) // 1-bit input: 3-state clock enable
-	);
-
-wire selectio_out;	
-wire selectio_clk_out;
-selectio_wiz_0
-   // width of the data for the system
- #( .SYS_W(1),
-   // width of the data for the device
-   .DEV_W (8))
-ass (
-  // From the device out to the system
-	.data_out_from_device(8'b1001_0010),
-	.data_out_to_pins(selectio_out),
-	.clk_in(clk_ddr_100),        // Single ended clock from IOB
-	.clk_div_out(selectio_clk_out),   // Slow clock output
-	.clk_reset(0),
-	.io_reset(phy_rst));
 endmodule
--- art/art.srcs/sources_1/ddr3_x16_phy.v
+++ art/art.srcs/sources_1/ddr3_x16_phy.v
@@ -1,23 +1,21 @@
 `timescale 1ns / 1ps
 
 module ddr3_x16_phy #(
-	parameter	REFCLK_FREQUENCY	= 200.0,
-	parameter	SIMULATION	= "FALSE"
+	parameter	REFCLK_FREQUENCY	= 200.0
 )(
-	input	i_clk_ui,	// 4:1 ratio between ui and ddr clocks
-	input	i_clk_ddr,
-	input	i_clk_ddr_90,
-	input	i_clk_ref,	// 200 or 300 MHz, see REFCLK_FREQUENCY, used for IDELAYCTRL
+	input	i_clk_ddr,	// chip clock frequency
+	input	i_clk_ddr_90,	// same but delayed by 90°, used to generate output DQS
+	input	i_clk_ref,	// 200 MHz, used for IDELAYCTRL, which controls taps for input DQS IDELAY
 	
-	input	i_phy_rst,	// active high reset for OSERDES, IDELAYCTRL, hold HIGH until clocks are ready (approx. 900 ns as per simulation)
+	input	i_phy_rst,	// active high reset for ODDR, OSERDES, ISERDES, IDELAYCTRL, hold HIGH until all clocks are generated
+	input	i_phy_tristate_en,	// DQ/DQS IOBUF/IOBUFDS tristate (1 = Z, 0 = output enabled)
 	
-	input	[127:0]	i128_phy_wrdata,
+	input	[127:0]	i128_phy_wrdata,	// eight words of write data for OSERDES (out of 8 for a total of BL8)
+	output	[63:0]	o64_phy_rddata,	// four words of read data from ISERDES (out of 8 for a total of BL8)
 	
-	input	[1:0]	i2_phy_dqs,
-	input	[1:0]	i2_dqs_iobuf_out_nen,
-	input	i_dq_oserdes_en,
-	
-	output	o_phy_idelay_rdy,
+	input 	i_phy_dq_oserdes_en,	// DQ OSERDES enable
+		
+	input	i_phy_dqs_oddr_d1en,	// DQS ODDR D1 input
 	
 	// CONNECTION TO DRAM
 	inout	[15:0]	io16_ddr_dq,
@@ -25,29 +23,22 @@ module ddr3_x16_phy #(
 	inout	[1:0]	io2_ddr_dqs_n,
 	
 	output	o_ddr_ck_p,
-	output	o_ddr_ck_n,
-	
-	output	o14_ddr_addr
-
+	output	o_ddr_ck_n
 );
-localparam tXPR = 1000;
 
-wire w_idelay_rdy;	// idelay ready output
-assign o_phy_idelay_rdy = w_idelay_rdy;
+wire	[1:0]	w2_dqs_rd;	// IOBUFDS -> IDELAY
+wire	[1:0]	w2_dqs_rd_delayed;	// IDELAY -> ISERDES; Delayed to middle of data eye
+wire	[1:0]	w2_dqs_wr;	// ODDR -> IOBUFDS
+wire	[1:0]	w2_dqs_oddr_d1en;	// Connected to ODDR D1 input. D2 is 1'b0.
+									// 1: ODDR output is i_clk_ddr_90
+									// 0: ODDR output is 1'b0.
+assign w2_dqs_oddr_d1en = {(2){i_phy_dqs_oddr_d1en}};
 
-wire	[1:0]	w2_dqs_in;
-wire	[1:0]	w2_dqs_in_delayed; // Delayed to middle of data eye
-wire	[1:0]	w2_dqs_out;	// LDQS [0] and UDQS [1]
-wire	[1:0]	w2_dqs_outen;
-assign w2_dqs_outen = i2_phy_dqs;
+wire	[1:0]	w2_tristate_en;	// DQ IOBUF and DQS IOBUFDS tristate
+assign w2_tristate_en = {(2){i_phy_tristate_en}};
 
-wire	[1:0]	w2_dqs_iobuf_out_nen;
-assign w2_dqs_iobuf_out_nen = i2_dqs_iobuf_out_nen;	// TODO: connect dq/dqs buffer output enable (likely module input?)
-
-wire	[15:0]	w16_dq_in;
-wire	[15:0]	w16_dq_out;
-wire	[15:0]	w16_4_dq_deser	[3:0];
-wire	[15:0]	w16_dq_iobuf_out_nen;
+wire	[15:0]	w16_dq_wr;	// OSERDES -> IOBUF
+wire	[15:0]	w16_dq_rd;	// IOBUF -> ISERDES
 
 genvar i; // loop variable for generate blocks
 
@@ -70,11 +61,11 @@ for (i = 0; i < 2; i = i+1) begin
 	IOBUFDS #(
 		.IOSTANDARD("DIFF_SSTL135")	// Specify the I/O standard
 	) iobufds_dqs_inst (
-		.O(w2_dqs_in[i]),	// Buffer output
+		.O(w2_dqs_rd[i]),	// Buffer output
 		.IO(io2_ddr_dqs_p[i]),	// Diff_p inout (connect directly to top-level port)
 		.IOB(io2_ddr_dqs_n[i]),	// Diff_n inout (connect directly to top-level port)
-		.I(w2_dqs_out[i]),	// Buffer input
-		.T(w2_dqs_iobuf_out_nen[i])	// 3-state enable input, high=input, low=output
+		.I(w2_dqs_wr[i]),	// Buffer input
+		.T(w2_tristate_en[i])	// 3-state enable input, high=input, low=output
 	);
 end
 endgenerate
@@ -87,17 +78,17 @@ for (i = 0; i < 2; i = i+1) begin
 	IDELAYE2 #(
 		.HIGH_PERFORMANCE_MODE("TRUE"), // Reduced jitter ("TRUE"), Reduced power ("FALSE")
 		.IDELAY_TYPE("FIXED"), // FIXED, VARIABLE, VAR_LOAD, VAR_LOAD_PIPE
-		.IDELAY_VALUE(16), // Input delay tap setting (0-31)
+		.IDELAY_VALUE(28), // Input delay tap setting (0-31)
 		.REFCLK_FREQUENCY(REFCLK_FREQUENCY) // IDELAYCTRL clock input frequency in MHz (190.0-210.0, 290.0-310.0).
 	) idelay_dqs_inst (
 		.CNTVALUEOUT(), // 5-bit output: Counter value output
-		.DATAOUT(w2_dqs_in_delayed[i]), // 1-bit output: Delayed data output
-		.C(i_clk_ui), // 1-bit input: Clock input
+		.DATAOUT(w2_dqs_rd_delayed[i]), // 1-bit output: Delayed data output
+		.C(i_clk_ddr_90), // 1-bit input: Clock input
 		.CE(1'b0), // 1-bit input: Active high enable increment/decrement input
 		.CINVCTRL(1'b0), // 1-bit input: Dynamic clock inversion input
 		.CNTVALUEIN(5'b0), // 5-bit input: Counter value input
 		.DATAIN(1'b0), // 1-bit input: Internal delay data input
-		.IDATAIN(w2_dqs_in[i]), // 1-bit input: Data input from the I/O
+		.IDATAIN(w2_dqs_rd[i]), // 1-bit input: Data input from the I/O
 		.INC(1'b0), // 1-bit input: Increment / Decrement tap delay input
 		.LD(1'b0), // 1-bit input: Load IDELAY_VALUE input
 		.LDPIPEEN(1'b0), // 1-bit input: Enable PIPELINE register to load data input
@@ -115,10 +106,10 @@ for (i = 0; i < 2; i = i+1) begin
 		.DDR_CLK_EDGE("SAME_EDGE"), // "OPPOSITE_EDGE" or "SAME_EDGE"
 		.INIT(1'b0) // Initial value of Q: 1'b0 or 1'b1
 	) oddr_dqs_inst (
-		.Q(w2_dqs_out[i]), // 1-bit DDR output
+		.Q(w2_dqs_wr[i]), // 1-bit DDR output
 		.C(i_clk_ddr_90), // 1-bit clock input
 		.CE(1'b1), // 1-bit clock enable input
-		.D1(w2_dqs_outen[i]), // 1-bit data input (positive edge)
+		.D1(w2_dqs_oddr_d1en[i]), // 1-bit data input (positive edge)
 		.D2(1'b0), // 1-bit data input (negative edge)
 		.R(i_phy_rst), // 1-bit reset
 		.S(1'b0) // 1-bit set
@@ -144,32 +135,33 @@ for (i = 0; i < 16; i = i+1) begin
 		.IOSTANDARD("SSTL135"),	// Specify the I/O standard
 		.SLEW("FAST")	// Specify the output slew rate
 	) iobuf_dq_inst (
-		.O(w16_dq_in[i]),	// Buffer output [signal coming into fpga]
+		.O(w16_dq_rd[i]),	// Buffer output [signal coming into fpga]
 		.IO(io16_ddr_dq[i]),	// Buffer inout port (connect directly to top-level port)
-		.I(w16_dq_out[i]),	// Buffer input [signal going out of fpga]
-		.T(w16_dq_iobuf_out_nen[i])	// 3-state enable input, high=input, low=output
+		.I(w16_dq_wr[i]),	// Buffer input [signal going out of fpga]
+		.T(w2_tristate_en[i/8])	// 3-state enable input, high=input, low=output
 	);
 end
 endgenerate
 
 /////////////////////////////////////////////////
-// DQ INPUT SERIAL-TO-PARALLEL
+// DQ ISERDES
 /////////////////////////////////////////////////
 generate
 for (i = 0; i < 16; i = i+1) begin
 	ISERDESE2 #(
 		.DATA_RATE("DDR"), // DDR, SDR
 		.DATA_WIDTH(4), // Parallel data width (2-8,10,14)
+						// In MEMORY + DDR mode, only DATA_WIDTH 4 supported, UG471 Table 3-3
 		.INTERFACE_TYPE("MEMORY"), // MEMORY, MEMORY_DDR3, MEMORY_QDR, NETWORKING, OVERSAMPLE
 		.IOBDELAY("NONE"), // NONE, BOTH, IBUF, IFD
-		.NUM_CE(1) // Number of clock enables (1,2)
+		.NUM_CE(2) // Number of clock enables (1,2)
 	) iserdes_dq_inst (
 		.O(), // 1-bit output: Combinatorial output
 		// Q1 - Q8: 1-bit (each) output: Registered data outputs
-		.Q1(w16_4_dq_deser[0][i]),
-		.Q2(w16_4_dq_deser[1][i]),
-		.Q3(w16_4_dq_deser[2][i]),
-		.Q4(w16_4_dq_deser[3][i]),
+		.Q1(o64_phy_rddata[i]),
+		.Q2(o64_phy_rddata[i+16]),
+		.Q3(o64_phy_rddata[i+32]),
+		.Q4(o64_phy_rddata[i+48]),
 		.Q5(),
 		.Q6(),
 		.Q7(),
@@ -184,20 +176,20 @@ for (i = 0; i < 16; i = i+1) begin
 		// SDR).
 		// CE1, CE2: 1-bit (each) input: Data register clock enable inputs
 		.CE1(1'b1),
-		.CE2(),
+		.CE2(1'b1),
 		.CLKDIVP(1'b0), // 1-bit input: TBD
 		// Clocks: 1-bit (each) input: ISERDESE2 clock input ports
-		.CLK(w2_dqs_in_delayed[i/8]), // 1-bit input: High-speed clock
-		.CLKB(~w2_dqs_in_delayed[i/8]), // 1-bit input: High-speed secondary clock
-		.CLKDIV(i_clk_ui), // 1-bit input: Divided clock
+		.CLK(w2_dqs_rd_delayed[i/8]), // 1-bit input: High-speed clock
+		.CLKB(~w2_dqs_rd_delayed[i/8]), // 1-bit input: High-speed secondary clock
+		.CLKDIV(i_clk_ddr), // 1-bit input: Divided clock
 		.OCLK(i_clk_ddr), // 1-bit input: High speed output clock used when INTERFACE_TYPE="MEMORY"
 		// Dynamic Clock Inversions: 1-bit (each) input: Dynamic clock inversion pins to switch clock polarity
-		.DYNCLKDIVSEL(DYNCLKDIVSEL), // 1-bit input: Dynamic CLKDIV inversion
-		.DYNCLKSEL(DYNCLKSEL), // 1-bit input: Dynamic CLK/CLKB inversion
+		.DYNCLKDIVSEL(1'b0), // 1-bit input: Dynamic CLKDIV inversion
+		.DYNCLKSEL(1'b0), // 1-bit input: Dynamic CLK/CLKB inversion
 		// Input Data: 1-bit (each) input: ISERDESE2 data input ports
-		.D(w16_dq_in[i]), // 1-bit input: Data input
+		.D(w16_dq_rd[i]), // 1-bit input: Data input
 		.DDLY(1'b0), // 1-bit input: Serial data from IDELAYE2
-		.OFB(OFB), // 1-bit input: Data feedback from OSERDESE2
+		.OFB(1'b0), // 1-bit input: Data feedback from OSERDESE2
 		.OCLKB(~i_clk_ddr), // 1-bit input: High speed negative edge output clock
 		.RST(i_phy_rst), // 1-bit input: Active high asynchronous reset
 		// SHIFTIN1, SHIFTIN2: 1-bit (each) input: Data width expansion input ports
@@ -206,13 +198,33 @@ for (i = 0; i < 16; i = i+1) begin
 	);
 end
 endgenerate
+//generate
+//for (i = 0; i < 16; i = i+1) begin
+//	IDDR #(
+//		.DDR_CLK_EDGE("SAME_EDGE_PIPELINED"), // "OPPOSITE_EDGE", "SAME_EDGE"
+//		// or "SAME_EDGE_PIPELINED"
+//		.INIT_Q1(1'b0), // Initial value of Q1: 1'b0 or 1'b1
+//		.INIT_Q2(1'b0), // Initial value of Q2: 1'b0 or 1'b1
+//		.SRTYPE("SYNC") // Set/Reset type: "SYNC" or "ASYNC"
+//	) IDDR_inst (
+//		.Q1(o32_phy_rddata[i]), // 1-bit output for positive edge of clock
+//		.Q2(o32_phy_rddata[i+16]), // 1-bit output for negative edge of clock
+//		.C(w2_dqs_rd_delayed[i/8]), // 1-bit clock input
+//		.CE(1'b1), // 1-bit clock enable input
+//		.D(w16_dq_rd[i]), // 1-bit DDR data input
+//		.R(i_phy_rst), // 1-bit reset
+//		.S(1'b0) // 1-bit set
+//	);
+//end
+//endgenerate
 
 /////////////////////////////////////////////////
-// DQ OUTPUT PARALLEL-TO-SERIAL
+// DQ OSERDES
 /////////////////////////////////////////////////
 generate
 for (i = 0; i < 16; i = i+1) begin
-	// for 8:1 data width, OQ latency is 3-5 CLK cycles (UG471)(?)
+	// For 8:1 data width, OQ latency is 3, 4, or 5 CLK cycles (UG471)(?)
+	// Simulation shows delay is 5 CLK cycles. ILA testing recommended.
 	OSERDESE2 #(
 		.DATA_RATE_OQ("DDR"), // DDR, SDR
 		.DATA_RATE_TQ("BUF"), // DDR, BUF, SDR
@@ -220,15 +232,15 @@ for (i = 0; i < 16; i = i+1) begin
 		.TRISTATE_WIDTH(1) // 3-state converter width (1,4)
 	) oserdes_dq_inst (
 		.OFB(), // 1-bit output: Feedback path for data
-		.OQ(w16_dq_out[i]), // 1-bit output: Data path output
+		.OQ(w16_dq_wr[i]), // 1-bit output: Data path output
 		// SHIFTOUT1 / SHIFTOUT2: 1-bit (each) output: Data output expansion (1-bit each)
 		.SHIFTOUT1(),
 		.SHIFTOUT2(),
 		.TBYTEOUT(), // 1-bit output: Byte group tristate
 		.TFB(), // 1-bit output: 3-state control
-		.TQ(w16_dq_iobuf_out_nen[i]), // 1-bit output: 3-state control
+		.TQ(), // 1-bit output: 3-state control
 		.CLK(i_clk_ddr), // 1-bit input: High speed clock
-		.CLKDIV(i_clk_ui), // 1-bit input: Divided clock
+		.CLKDIV(i_clk_ddr), // 1-bit input: Divided clock
 		// D1 - D8: 1-bit (each) input: Parallel data inputs (1-bit each)
 		.D1(i128_phy_wrdata[i+16*0]),
 		.D2(i128_phy_wrdata[i+16*1]),
@@ -238,13 +250,13 @@ for (i = 0; i < 16; i = i+1) begin
 		.D6(i128_phy_wrdata[i+16*5]),
 		.D7(i128_phy_wrdata[i+16*6]),
 		.D8(i128_phy_wrdata[i+16*7]),
-		.OCE(i_dq_oserdes_en),//1'b1), // 1-bit input: Output data clock enable
+		.OCE(i_phy_dq_oserdes_en),//1'b1), // 1-bit input: Output data clock enable
 		.RST(i_phy_rst), // 1-bit input: Reset
 		// SHIFTIN1 / SHIFTIN2: 1-bit (each) input: Data input expansion (1-bit each)
 		.SHIFTIN1(1'b0),
 		.SHIFTIN2(1'b0),
 		// T1 - T4: 1-bit (each) input: Parallel 3-state inputs
-		.T1(w2_dqs_iobuf_out_nen[i/8]),
+		.T1(1'b0),
 		.T2(),//w2_dqs_iobuf_out_nen[i/8]),
 		.T3(),//w2_dqs_iobuf_out_nen[i/8]),
 		.T4(),//w2_dqs_iobuf_out_nen[i/8]),
@@ -253,5 +265,21 @@ for (i = 0; i < 16; i = i+1) begin
 	);
 end
 endgenerate
+//generate
+//for (i = 0; i < 16; i = i+1) begin
+//	ODDR #(
+//		.DDR_CLK_EDGE("SAME_EDGE"), // "OPPOSITE_EDGE" or "SAME_EDGE"
+//		.INIT(1'b0) // Initial value of Q: 1'b0 or 1'b1
+//	) oddr_dq_inst (
+//		.Q(w16_dq_wr[i]), // 1-bit DDR output
+//		.C(i_clk_ddr), // 1-bit clock input
+//		.CE(i_phy_dq_oddr_en), // 1-bit clock enable input
+//		.D1(i32_phy_wrdata[i]), // 1-bit data input (positive edge)
+//		.D2(i32_phy_wrdata[i+16]), // 1-bit data input (negative edge)
+//		.R(i_phy_rst), // 1-bit reset
+//		.S(1'b0) // 1-bit set
+//	);
+//end
+//endgenerate
 
 endmodule
