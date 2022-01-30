daf26920b7b65ead257c21a6689001dd869b7df4
Jari Beguš <someone755@users.noreply.github.com>
2022-01-30 02:19:07 +0100

    IP: add clk_wiz_1 for custom controller clocking
    * Mostly committing to clean up git status

---
+++ art/art.ip_user_files/ip/clk_wiz_1/clk_wiz_1_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+-- Date        : Fri Jan 28 01:45:54 2022
+-- Host        : silver-surfer running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               C:/Users/Jari/OneDrive/work/mag/vivado-proj/art_ip/clk_wiz_1/clk_wiz_1_stub.vhdl
+-- Design      : clk_wiz_1
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7s50csga324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_wiz_1 is
+  Port ( 
+    clk_out1_ddr : out STD_LOGIC;
+    clk_out2_ddr_270 : out STD_LOGIC;
+    clk_out3_ref : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end clk_wiz_1;
+
+architecture stub of clk_wiz_1 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1_ddr,clk_out2_ddr_270,clk_out3_ref,reset,locked,clk_in1";
+begin
+end;
---
+++ art/art.ip_user_files/sim_scripts/clk_wiz_1/xcelium/run.f
@@ -0,0 +1,15 @@
+-makelib xcelium_lib/xpm -sv \
+  "C:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "C:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib xcelium_lib/xpm \
+  "C:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../art_ip/clk_wiz_1/clk_wiz_1_clk_wiz.v" \
+  "../../../../../art_ip/clk_wiz_1/clk_wiz_1.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
