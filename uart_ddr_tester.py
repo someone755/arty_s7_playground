6ca732be57370b5dfd53e90a6d6d5d181fbd811f
Jari Beguš <jari.begus@gmail.com>
2022-10-17 19:49:21 +0200

    ddr_tester.py: Round off time format of script output to 2 decimals

--- uart_ddr_tester.py
+++ uart_ddr_tester.py
@@ -6,7 +6,7 @@ if True: ## Global script settings
     # Baud rate set in Artix bitstream
     baudRate = 3000000
     # SDRAM frequency
-    ddrFreq = 125000000
+    ddrFreq = 325000000 #300000000
     # Size of data to write to DDR3, in bytes
     # Must be multiple of 16 bytes (BL8 × 16-bit bus = 16 byte words),
     # up to module size of 2 Gbit or 2**28 = 268435456 bytes
@@ -35,7 +35,7 @@ if True: ## Library imports
 
 if True: ## Function declarations
     def genDataTx():
-        print('[{}] I: Creating new blob of size'.format(time.time()-t), sizeOfData)
+        print('[{}] I: Creating new blob of size'.format(round(time.time()-t, 2)), sizeOfData)
         dataTxL = os.urandom(sizeOfData)
         open('dataTx.bin', 'wb').write(dataTxL)
     
@@ -44,17 +44,17 @@ t = time.time()
 
 if True: ## Check variable sizes
     if sizeOfData % 16 :
-        print('[{}] E: Data size must be multiple of 16!'.format(time.time()-t))
+        print('[{}] E: Data size must be multiple of 16!'.format(round(time.time()-t, 2)))
         quit()
     if startWrAddr % 8 :
-       print('[{}] E: Address offset must be multiple of 8!'.format(time.time()-t))
+       print('[{}] E: Address offset must be multiple of 8!'.format(round(time.time()-t, 2)))
        quit()
 
 if True: ## Look for data blob on disk, generate if not found
     # Check if file exists
     if os.path.exists('dataTx.bin'):
         if os.path.getsize('dataTx.bin') == sizeOfData:
-            print('[{}] I: Found blob of size'.format(time.time()-t), sizeOfData)
+            print('[{}] I: Found blob of size'.format(round(time.time()-t, 2)), sizeOfData)
         else:
             genDataTx()
     else:
@@ -72,15 +72,15 @@ if flushDDR: ## Write up to 32 0xFF bytes (16*0xFF => RD op)
     ser.set_buffer_size(rx_size = sizeOfData, tx_size = sizeOfData)
 
     # Make sure DDR is empty by flushing with EOF word
-    print('[{}] I: Flushing DDR with EOF word...'.format(time.time()-t))
+    print('[{}] I: Flushing DDR with EOF word...'.format(round(time.time()-t, 2)))
     for i in range(0,33):
         ser.write(b'\x66')
         ackByte = ser.read()
         if (ackByte == b'\x8a'):
-            print('[{}] I: DDR now empty. ACK ='.format(time.time()-t), ackByte)
+            print('[{}] I: DDR now empty. ACK ='.format(round(time.time()-t, 2)), ackByte)
             break
     if ackByte == b'':
-        print('[{}] E: No response from FPGA!'.format(time.time()-t))
+        print('[{}] E: No response from FPGA!'.format(round(time.time()-t, 2)))
         quit()
     time.sleep(1)
 
@@ -97,7 +97,7 @@ if setAddr: ## Set starting addr, [127:64] => 0xaa, [27:0] => startWrAddr
     ser.write(int(startWrAddr).to_bytes(4, 'big'))
 
 if True: ## Send data
-    print('[{}] I: TX data blob...'.format(time.time()-t))
+    print('[{}] I: TX data blob...'.format(round(time.time()-t, 2)))
     time1 = time.time()
     if (waitForAck): ## 16 bytes at a time
         i=int(0)
@@ -109,10 +109,10 @@ if True: ## Send data
                 i = i+16
                 ser.write(dataTx[i:i+16])
             elif ackByte == b'\x88':
-                print(i/2,"[{}] E: RX byte b'x8a' != ".format(time.time()-t), ackByte)
+                print(i/2,"[{}] E: RX byte b'x8a' != ".format(round(time.time()-t, 2)), ackByte)
                 time.sleep(1)
             elif ackByte == b'':
-                print(i/2,"[{}] E: No response from FPGA!".format(time.time()-t))
+                print(i/2,"[{}] E: No response from FPGA!".format(round(time.time()-t, 2)))
                 quit()
             
     else: ## All at once
@@ -120,7 +120,7 @@ if True: ## Send data
         
     time2 = time.time()
     if (time2-time1) != 0:
-        print('[{}] I: TX effective rate:'.format(time2-t),sizeOfData*8/(time2-time1)/1000,'kbit/s')
+        print('[{}] I: TX effective rate:'.format(round(time2-t,2)),sizeOfData*8/(time2-time1)/1000,'kbit/s')
     time.sleep(2)
 
 if True: ## Close and reopen serial port; this drops the current TX buffer
@@ -137,36 +137,36 @@ if True: ## Send read request along with start and end address
     time.sleep(0.1)
 
 if True: ## Receive data of len(sizeOfData)
-    print('[{}] I: Receiving data from DDR...'.format(time.time()-t))
+    print('[{}] I: Receiving data from DDR...'.format(round(time.time()-t, 2)))
     time1 = time.time()
     dataRx = ser.read(sizeOfData)
     time2 = time.time()
     if (time2-time1) != 0:
-        print('[{}] I: RX effective rate:'.format(time2-t),len(dataRx)*8/(time2-time1)/1000,'kbit/s')
+        print('[{}] I: RX effective rate:'.format(round(time2-t,2)),len(dataRx)*8/(time2-time1)/1000,'kbit/s')
     open('dataRx.bin', 'wb').write(dataRx)
 
 if True: ## Generate MD5 for RX data
-    print('[{}] I: Generating RX data MD5...'.format(time.time()-t))
+    print('[{}] I: Generating RX data MD5...'.format(round(time.time()-t, 2)))
     dataRxHash = hashlib.md5(dataRx).hexdigest()
     dataRxHashSeq = hashlib.md5(dataRx[0:2048]).hexdigest()
 
 if True: ## Compare TX and RX data hashes
     if dataRxHash == dataTxHash :
-        print('[{}] I: Success! File hash match!'.format(time.time()-t))
+        print('[{}] I: Success! File hash match!'.format(round(time.time()-t, 2)))
     else:
-        print('[{}] E: File hash mismatch!'.format(time.time()-t))
+        print('[{}] E: File hash mismatch!'.format(round(time.time()-t, 2)))
         print("RX:", len(dataRx), "TX:", len(dataTx))
         
 if seqTest & (dataRxHash == dataTxHash): ## Read the first column sequentially if connection works
     for i in range (0,16):
         ser.write(b'\x72')
     time.sleep(0.1)
-    print('[{}] I: Testing sequential speeds...'.format(time.time()-t))
+    print('[{}] I: Testing sequential speeds...'.format(round(time.time()-t, 2)))
     dataRx_seq = ser.read(2048)
     seqTime = int.from_bytes(ser.read(4),"big")
     #print((ddr_seq_rd_time))
-    print('[{}] I: Sequential read effective rate:'.format(time2-t),2048/2*ddrFreq/1e6/seqTime, "MB/s")
-    print('[{}] I: Theoretical maximum throughput at frequency is:'.format(time.time()-t),ddrFreq*4/1e6, "MB/s")
+    print('[{}] I: Sequential read effective rate:'.format(round(time2-t,2)),2048/2*ddrFreq/1e6/seqTime, "MB/s")
+    print('[{}] I: Theoretical maximum throughput at frequency is:'.format(round(time.time()-t, 2)),ddrFreq*4/1e6, "MB/s")
     open('dataRx_seq.bin', 'wb').write(dataRx_seq)
     
 ser.close() # Close port for another run/program
